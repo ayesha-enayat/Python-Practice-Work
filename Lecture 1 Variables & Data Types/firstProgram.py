896e395437b7bf310f4a9adf461d1516e61d48ad
ayeshaenayat <ayeshaenayat123enayat@gmail.com>
2025-01-07 19:52:25 +0500

    learn datatypes

--- Lecture 1 Variables & Data Types/firstProgram.py
+++ Lecture 1 Variables & Data Types/firstProgram.py
@@ -1,1 +1,82 @@
+# Print Statement
 print("Hello World!")
+print(23)
+print("My name is Ayesha","I am 0 years old")
+print(35+5)
+print(20-2)
+
+#Variables
+name="Ayesha Enayat"
+print(name)
+age=1
+print(age)
+price=25.88
+print(price)
+print("My name is: ",name)
+print("My age is: ",age)
+
+age2=age
+print(age2)
+
+#Rules for Identifiers
+#1. It must start with a letter or underscore
+#2. It can only contain letters, digits, and underscores
+#3. It cannot start with a digit
+#4. It cannot contain spaces
+#5. It cannot contain special characters
+#6. It is case sensitive
+#7. It cannot be a keyword
+#8. It cannot be a reserved word
+
+
+#DataTypes
+#Integer (+ve,-ve,0)
+#Float  (9.6,2.2)
+#string ('s',"s","'s'")
+#Boolean (True or False)
+#None (a=none)
+print(type(name))
+print(type(age))
+print(type(price))
+
+
+old=False
+a=None
+print(type(old))
+print(type(a))
+
+
+#keywords
+#and,as,assert,async,await,break,class,continue,def,del, and so on
+
+
+#Sum of two numbers
+a=1
+b=2
+sum=a+b
+print(sum)
+
+
+#single line comment
+"""
+Multi line 
+comments
+"""
+
+
+#Types Of Operators
+#Arithmetic Operator (+,-,*,/,%,**)
+#Assignment Operator (=,+=,-=,*=,/=,%=,**=)
+#Comparison Operator (==,!=,>,<,>=,<=)
+#Logical Operator (and,or,not)
+c=5
+d=5
+print(c+d)
+print(c-d)
+print(c*d)
+print(c/d)
+print(c%d)
+print(c**d)
+
+
+
